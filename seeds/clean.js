b7381ca8ad2dade25d6c6acf04bb948a650b0c39
Peter <peter.lim91@gmail.com>
2021-01-21 22:22:43 +1300

    populate seeds

---
+++ seeds/clean.js
@@ -0,0 +1,9 @@
+exports.seed = function (knex, Promise) {
+  const empty = table =>
+    () => knex(table).del()
+
+  return empty('playlistscontents')()
+  .then(empty('playlists'))
+  .then(empty('songs'))
+  .then(empty('users'))
+}
---
+++ seeds/users.js
@@ -0,0 +1,15 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('table_name').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('table_name').insert([
+        {id: 1, name: 'multi', bio:''},
+        {id: 2, name: 'pete', bio:''},
+        {id: 3, name: 'peter', bio:''},
+        {id: 4, name: 'steve', bio:''},
+        {id: 5, name: 'tausani', bio:''}
+      ]);
+    });
+};
