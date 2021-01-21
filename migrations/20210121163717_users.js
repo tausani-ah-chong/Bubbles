fcc27d237322a1509a9f766a8b115c41ed1b0ddf
Tausani Ah Chong <tausani93@gmail.com>
2021-01-21 21:31:15 +1300

    add tables and clean seed

--- migrations/20210121163717_users.js
+++ migrations/20210121163717_users.js
@@ -1,7 +1,12 @@
 exports.up = knex => {
+  return knex.schema.createTable('users', (table => {
+    table.increments('id').primary()
+    table.string('name')
+    table.string('bio')
+  }))
   
 }
 
 exports.down = knex => {
-  
+  return knex.schema.dropTable('users')
 }
--- migrations/20210121164333_playlists.js
+++ migrations/20210121164333_playlists.js
@@ -1,7 +1,13 @@
 exports.up = knex => {
-  
+  return knex.schema.createTable('playlists', (table => {
+    table.increments('id').primary()
+    table.string('playlist_name')
+    table.integer('playlist_id')
+    table.integer('song_id').references('song.id')
+    table.integer('user_id').references('user.id')
+  }))
 }
 
 exports.down = knex => {
-  
+  return knex.schema.dropTable('playlists')
 }
