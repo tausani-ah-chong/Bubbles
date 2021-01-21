006c0c1c338ea3e7f129b061874a56b306500c96
Tausani Ah Chong <tausani93@gmail.com>
2021-01-22 11:02:58 +1300

    add img to user seed

--- seeds/1-users.js
+++ seeds/1-users.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   return knex('users').insert([
-    {id: 1, name: 'multi', city:'Apia'},
-    {id: 2, name: 'tausani', city:'Capital Of The Pacific'},
-    {id: 3, name: 'pete', city:'Hook Strait'},
-    {id: 4, name: 'steve', city:'Hypetown'},
-    {id: 5, name: 'peter', city:'Hackysacksville'}
+    {id: 1, name: 'multi', city:'Apia', img: '' },
+    {id: 2, name: 'tausani', city:'Capital Of The Pacific', img: '' },
+    {id: 3, name: 'pete', city:'Hook Strait', img: '' },
+    {id: 4, name: 'steve', city:'Hypetown', img: '' },
+    {id: 5, name: 'peter', city:'Hackysacksville', img: '' }
   ])
 }
