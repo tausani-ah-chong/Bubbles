751e3f8e415925244dfa6cfe5019acd08f63afdf
steve-kim-96 <soungku@hotmail.com>
2021-01-22 10:54:26 +1300

    create function to join users table with playlists table

--- db.js
+++ db.js
@@ -20,3 +20,14 @@ function getPlaylist (db = database) {
 function getSong (db = database) {
   
 }
+
+function joinUsersWithPlaylist(id, db = database) {
+  return db('users')
+    .join('playlists', 'users.id', 'user_id')
+    .where('users.id', id)
+    .select('users.id', 'users.name',  'playlists.name')
+    .first()
+    .then(result => result)
+}
+
+joinUsersWithPlaylist(1)
