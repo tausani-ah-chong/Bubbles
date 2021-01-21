2e321c73f1d8972d14e998363f0f9712033f5fc6
Tausani Ah Chong <tausani93@gmail.com>
2021-01-22 11:29:34 +1300

    edit playlists seed

--- seeds/3-playlists.js
+++ seeds/3-playlists.js
@@ -1,15 +1,15 @@
 
 exports.seed = function(knex) {
   return knex('playlists').insert([
-    {id: 1, playlist_name: 'Multi 1', user_id: 1 },
-    {id: 2, playlist_name: 'Multi 2', user_id: 1 },
-    {id: 4, playlist_name: 'Sani 1', user_id: 2 },
-    {id: 3, playlist_name: 'Sani 2', user_id: 2 },
-    {id: 5, playlist_name: 'House', user_id: 3 },
-    {id: 6, playlist_name: 'Rock', user_id: 3 },
-    {id: 7, playlist_name: 'Peter 1', user_id: 4 },
-    {id: 8, playlist_name: 'Peter 2', user_id: 4 },
-    {id: 9, playlist_name: 'Disney', user_id: 5 },
-    {id: 10, playlist_name: 'Not Disney', user_id: 5 }
+    {id: 1, name: 'Multi 1', user_id: 1 },
+    {id: 2, name: 'Multi 2', user_id: 1 },
+    {id: 4, name: 'Sani 1', user_id: 2 },
+    {id: 3, name: 'Sani 2', user_id: 2 },
+    {id: 5, name: 'House', user_id: 3 },
+    {id: 6, name: 'Rock', user_id: 3 },
+    {id: 7, name: 'Peter 1', user_id: 4 },
+    {id: 8, name: 'Peter 2', user_id: 4 },
+    {id: 9, name: 'Disney', user_id: 5 },
+    {id: 10, name: 'Not Disney', user_id: 5 }
   ])
 }
