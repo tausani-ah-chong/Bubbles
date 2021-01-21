0f0a4dfc1ab0057b7caaaa8fb657e51e7ffd50a7
Tausani Ah Chong <tausani93@gmail.com>
2021-01-22 07:29:27 +1300

    add initial db.js funcs

--- db.js
+++ db.js
@@ -3,6 +3,20 @@ const config = require('./knexfile').development
 const database = knex(config)
 
 module.exports = {
+  // getUsers,
+  // getPlaylist,
+  // getSong,
+}
+
+function getUsers (db = database) {
+  return db('users')
+    .select()
+}
+
+function getPlaylist (db = database) {
 
 }
 
+function getSong (db = database) {
+  
+}
