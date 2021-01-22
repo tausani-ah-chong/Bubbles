b2ddd7435238c55ed26ad8380823acd89b49e6cb
Tausani Ah Chong <tausani93@gmail.com>
2021-01-22 13:48:09 +1300

    update db.js

--- db.js
+++ db.js
@@ -6,6 +6,7 @@ module.exports = {
   getUsers,
   getSongsArtist,
   joinUsersWithPlaylist,
+  insertUser,
 }
 
 function getUsers (db = database) {
@@ -24,7 +25,21 @@ function joinUsersWithPlaylist(id, db = database) {
   return db('users')
     .join('playlists', 'users.id', 'playlists.user_id')
     .where('users.id', id)
-    .select('users.id', 'users.name',  'playlists.name')
-    .then(result => result)
+    .select('users.id', 'users.name as userName',  'playlists.name as playlistName', 'img', 'city')
+    .then(result => {
+      return {
+        img: result[0].img,
+        userName: result[0].userName,
+        city: result[0].city,
+        playlists: result.map(post => ({
+          playlistName: post.playlistName,
+          playlistId: post.playlistId
+        }))
+      }
+    })
 }
 
+function insertUser (input, db = database) {
+    return db('users')
+      .insert( { name: input.name, city: input.city, img: input.img } )
+}
