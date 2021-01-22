b164a3c8b01d5f05cfff368b370f65168c0ebc31
multi <multiahfoon@gmail.com>
2021-01-22 14:59:00 +1300

    fix front end

--- seeds/3-playlists.js
+++ seeds/3-playlists.js
@@ -1,8 +1,8 @@
 
 exports.seed = function(knex) {
   return knex('playlists').insert([
-    {id: 1, name: 'Multi 1', user_id: 1 },
-    {id: 2, name: 'Multi 2', user_id: 1 },
+    {id: 1, name: 'Study Music', user_id: 1 },
+    {id: 2, name: 'Chill', user_id: 1 },
     {id: 4, name: 'Sani 1', user_id: 2 },
     {id: 3, name: 'Sani 2', user_id: 2 },
     {id: 5, name: 'House', user_id: 3 },
