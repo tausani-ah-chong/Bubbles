f663adb94d9cc7f6b1439349be3f1b663a2c8c4d
Peter <peter.lim91@gmail.com>
2021-01-22 13:32:11 +1300

    add playlist route

--- users.js
+++ users.js
@@ -17,5 +17,25 @@ router.get('/', (req, res) => {
     
 })
 
+router.get('/playlist/:id', (req, res) => {
+    return db.getSongsArtist(parseInt(req.params.id))
+    .then(result => {
+        const playlistName = result[0].name;
+        const songs = (result.map((song) => {
+            return {
+                name: song.song_name,
+                artist: song.artist
+            }
+        }))
+        const viewData = {
+            playlistName,
+            songs
+        }
+        // console.log(viewData)
+        res.render('playlist', viewData)
+        return null
+    })
+    .catch((err) => console.log(err))
+})
 
 module.exports = router
