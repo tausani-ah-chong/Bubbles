90cff8e8b49d52c5d61cd73e2c224c66c41c8362
Pete Angjelevski <p.angjelevski@gmail.com>
2021-01-22 13:38:34 +1300

    add user/:id route

--- users.js
+++ users.js
@@ -11,10 +11,24 @@ const router = express.Router()
 router.get('/', (req, res) => {
    return  db.getUsers()
     .then( viewData => {
-        console.log(viewData)
        return res.render('home', {viewData: viewData})
     })
+    .catch(err => {
+        res.status(500).send('DATABASE ERROR: ' + err.message)
+      })
     
+})
+
+router.get ('/user/:id', (req, res) => {
+    const id = Number(req.params.id)
+    return db.joinUsersWithPlaylist(id)
+        .then(viewData => {
+            console.log(viewData)
+           return  res.render('user', {viewData: viewData})
+        })
+        .catch(err => {
+            res.status(500).send('DATABASE ERROR: ' + err.message)
+          })
 })
 
 
